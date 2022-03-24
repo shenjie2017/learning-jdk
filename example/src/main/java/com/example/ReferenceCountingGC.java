e8115e5c18cbe590d3aa816aa82b74fcb17a5f28
Jason <1075850619@qq.com>
2022-03-24 11:42:52 +0800

    增加了存活判定算法中引用代码的gc日志输出部分

--- example/src/main/java/com/example/ReferenceCountingGC.java
+++ example/src/main/java/com/example/ReferenceCountingGC.java
@@ -4,8 +4,10 @@ public class ReferenceCountingGC {
     public Object instance = null;
     private static final int _1MB=1024*1024;
 
-    private byte[] bigSize = new byte[2*_1MB];
+    private byte[] bigSize = new byte[20*_1MB];
 
+    //打印gc日志
+    //vm args: -verbose:gc
     public static void main(String[] args) {
         ReferenceCountingGC objA=new ReferenceCountingGC();
         ReferenceCountingGC objB=new ReferenceCountingGC();
